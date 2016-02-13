9cbe8eab5755e301c1da954f187d9efbd781bb70
Bin Wang <bw2491@columbia.edu>
2016-02-13 18:04:49 -0500

    update yahoo crawler, bug fix

--- Yahoo_Price.py
+++ Yahoo_Price.py
@@ -1,18 +1,26 @@
-from yahoo_finance import Share
 import time
+from yahoo_finance import Share
 
 # Download price of a company
-price = Share("COMPANY_NAME")
+price = Share("YHOO")
+
 # New a file to record its current price
-f = open("COMPANY_NAME" + "_STOCKPRICE", "wb")
+f = open("COMPANY_NAME" + "_STOCKPRICE.txt", "wb")
+
+f.write(str(price.get_info()) + "\n")
+
 # Get open stock value
-f.write(str(price.get_open()) +  " at stock open\n”)
+f.write(str(price.get_open()) + "\n")
+
 # Get next price
 f.write(str(price.get_price()) + " at " + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())) + "\n")
+
 for i in range(0, 28):
 	# Sleep one min
-	time.sleep(60)
+	time.sleep(1)
 	# Get next price
 	f.write(str(price.get_price()) + " at " + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())) + "\n")
+
 f.write("Complete!")
+
 f.close()
